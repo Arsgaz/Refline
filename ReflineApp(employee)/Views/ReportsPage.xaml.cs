0d5498b3d741b897cda0abab80b232f2d5c0391d
Arseniy <arseniy@Arseniys-MacBook-Air.local>
2026-04-04 17:58:25 +0300

    graph refresh

--- ReflineApp(employee)/Views/ReportsPage.xaml.cs
+++ ReflineApp(employee)/Views/ReportsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
+using System.Windows.Threading;
 using Refline.ViewModels;
 
 namespace Refline.Views
@@ -10,24 +11,45 @@ namespace Refline.Views
         public ReportsPage()
         {
             InitializeComponent();
+            Loaded += ReportsPage_Loaded;
         }
 
         private void ShowReport_Click(object sender, RoutedEventArgs e)
         {
             ShowReportButton.Visibility = Visibility.Collapsed;
             ReportArea.Visibility = Visibility.Visible;
+            RefreshChartsAfterLayout();
+
+            var fade = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromMilliseconds(350)));
+            ReportArea.BeginAnimation(UIElement.OpacityProperty, fade);
+        }
+
+        private void ReportsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ReportArea.Visibility == Visibility.Visible)
+            {
+                RefreshChartsAfterLayout();
+            }
+        }
+
+        private void RefreshChartsAfterLayout()
+        {
+            ReportArea.UpdateLayout();
 
             if (DataContext is MainViewModel mainViewModel)
             {
                 mainViewModel.RefreshReportData();
             }
 
-            ReportArea.UpdateLayout();
-            CategoryPieChart.UpdateLayout();
-            TopApplicationsChart.UpdateLayout();
-
-            var fade = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromMilliseconds(350)));
-            ReportArea.BeginAnimation(UIElement.OpacityProperty, fade);
+            Dispatcher.BeginInvoke(
+                DispatcherPriority.Render,
+                new Action(() =>
+                {
+                    CategoryPieChart.UpdateLayout();
+                    TopApplicationsChart.UpdateLayout();
+                    CategoryPieChart.InvalidateVisual();
+                    TopApplicationsChart.InvalidateVisual();
+                }));
         }
     }
 }
